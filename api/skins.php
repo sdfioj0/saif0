62c14c41a53723ad13a0dabe4514a33bedbd1dc6
sdfioj0 <men056466@gmail.com>
2025-09-14 19:50:08 +0300

    Update skins.php

--- api/skins.php
+++ api/skins.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header("Content-Type: application/json; charset=UTF-8");
 
 try {
-    $url = 'https://sdfioj0.github.io/saif0/api/skins.json';
+    $file = 'api/skins.json';
     $json = file_get_contents($url);
     
     if ($json === false) {
